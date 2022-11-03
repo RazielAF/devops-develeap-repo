b7eef729e46097e97c9fa0e5d99eaa19e1c89f59
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-11-03 14:58:41 +0100

    add ted-search and cowsay with terraform and cert

---
+++ cert-manager-terraform-cowsay/init_cert.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+openssl req -nodes -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -subj "/C=PL/ST=Mazowieckie/L=Warsaw/O=develeap-bootcamp/OU=devops/CN=maciekgroszyk/emailAddress=maciej.groszyk8@gmail.com"
